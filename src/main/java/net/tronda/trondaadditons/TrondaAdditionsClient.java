a3c406d2895cd75aa50829917a72519ab52e369e
tronda_ <trondercom@gmail.com>
2026-08-30 08:53:11 +0200

    Initial commit

---
+++ src/main/java/net/tronda/trondaadditons/TrondaAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.tronda.trondaadditons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrondaAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
